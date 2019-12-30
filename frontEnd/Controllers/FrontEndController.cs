f2feade383a6dc8af04e725a4aa2df78c4261550
AndreasM009 <andreas.mock@live.com>
2019-12-30 15:41:17 +0100

    created demo services and frontend

---
+++ frontEnd/Controllers/FrontEndController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace frontEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FrontEndController : ControllerBase
+    {
+        [HttpGet("serviceA")]
+        public async Task<string> GetServiceA()
+        {
+            var url = $"http://localhost:3500/v1.0/invoke/serviceaapp/method/servicea";
+            var client = new HttpClient();
+            return await client.GetStringAsync(url);
+        }
+
+        [HttpGet("serviceB")]
+        public async Task<string> GetServiceB()
+        {
+            var url = $"http://localhost:3500/v1.0/invoke/servicebapp/method/serviceb";
+            var client = new HttpClient();
+            return await client.GetStringAsync(url);
+        }
+
+        [HttpGet("serviceC")]
+        public async Task<string> GetServiceC()
+        {
+            var url = $"http://localhost:3500/v1.0/invoke/servicecapp/method/servicec";
+            var client = new HttpClient();
+            return await client.GetStringAsync(url);
+        }
+    }
+}
---
+++ serviceC/Controllers/ServiceCController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace serviceC.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServiceCController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var random = new Random();
+            var sec = random.Next(500, 4000);
+
+            await Task.Delay(sec);
+
+            return "Hello World, ServiceC";
+        }
+    }
+}
